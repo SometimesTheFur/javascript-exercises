5b38f898a34184e4552b3cbf85ef7038d7db4e4b
SometimesTheFur <timothymahr@gmail.com>
2020-05-02 16:39:05 -0400

    Took out nested func. 0 Failures

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,23 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str,i) {
+    let hey = ''
+    if (i < 0) return 'ERROR';
+    if (i === 0) return ''
+    if (i > 0) return str.repeat(i);
 
-    function strings(str, i){
-        let hey = ''
-            if (i < 0) return 'ERROR';
-            if (i === 0) return ''
-            if (i > 0) return str.repeat(i);
+    while(i <= 10) {
+        hey += str;
+        i++;
+    return hey;
+    }
 
-        while(i <= 10) {
-            hey += str;
-            i++;
-
-        }
-        return hey;
-    }   
-    strings('hey',-1); 
-    strings('hey',0);
-    strings('hey',1);
-    strings('hey',3);
-    strings('hey',10);
-
+    repeatString('hey',-1); 
+    repeatString('hey',0);
+    repeatString('hey',1);
+    repeatString('hey',3);
+    repeatString('hey',10);
 }
 module.exports = repeatString
