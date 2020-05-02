778e452098744c90976202e42d3ae3fc1342fc81
SometimesTheFur <timothymahr@gmail.com>
2020-05-02 16:09:00 -0400

    Got the param to work, but still only 1 result at a time

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,9 +1,23 @@
 const repeatString = function() {
-    let hey = ''
-    function strings(text){
-        text = hey.concat('hey');
-        return hey
-    }
-    return strings(hey);
+
+    function strings(str, i){
+        let hey = ''
+            if (i < 0) return 'ERROR';
+            if (i === 0) return ''
+            if (i > 0) return str.repeat(i);
+
+        while(i <= 10) {
+            hey += str;
+            i++;
+
+        }
+        return hey;
+    }   
+    strings('hey',-1); 
+    strings('hey',0);
+    strings('hey',1);
+    strings('hey',3);
+    strings('hey',10);
+
 }
 module.exports = repeatString
