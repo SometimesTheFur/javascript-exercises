ce8ba0c68b4e1bf19c57254a0aabbe23d1b83093
SometimesTheFur <timothymahr@gmail.com>
2020-05-05 15:49:59 -0400

    All but type of test 0

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,7 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    //if (typeOf(min) !== Number || typeOf(max) !== Number) return "ERROR";
+    if (min > max) {
+        let x = min;
+        min = max;
+        max = x;
+    }
 
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+         sum += i;       
+    }
+    return sum;
 }
-sumAll(1, 4);           //10
+sumAll([1, 4]);           //10
 sumAll(1, 4000);        //8002000
 sumAll(123, 1);         //7626
 sumAll(-10, 4);         //ERROR
