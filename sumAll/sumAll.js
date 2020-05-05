212035e994be45bcc34029f5cbfa758d95aba4ed
SometimesTheFur <timothymahr@gmail.com>
2020-05-05 13:32:03 -0400

    built input args

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function() {
 
 }
-
+sumAll(1, 4);           //10
+sumAll(1, 4000);        //8002000
+sumAll(123, 1);         //7626
+sumAll(-10, 4);         //ERROR
+sumAll(10, "90");       //ERROR
+sumAll(10, [90, 1]);    //ERROR
 module.exports = sumAll
