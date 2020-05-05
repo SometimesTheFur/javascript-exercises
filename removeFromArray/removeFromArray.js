b63d3cf73d627fd08c24d903f911db9f5f8d4153
SometimesTheFur <timothymahr@gmail.com>
2020-05-04 20:04:02 -0400

    Tests 0 Failures

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function([array]) {
+const removeFromArray = function(arr1,...args) {
 
+    return arr1.filter(item => !args.includes(item));
 }
-removeFromArray([1, 2, 3, 4]);
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+
 module.exports = removeFromArray
