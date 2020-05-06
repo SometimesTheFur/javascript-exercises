1708706c57a2b6422612f598c8cdad6a084dc154
SometimesTheFur <timothymahr@gmail.com>
2020-05-06 13:46:15 -0400

    Tests 0 Failures

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(degsF) {
+  let toC = ((degsF - 32) * 5/9).toFixed(1);
+  return parseFloat(toC);
 }
+//[°C] = ([°F] − 32) × ​5⁄9
+ftoc(32); //0
+ftoc(100); //37.8
+ftoc(-100); //-73.3
 
-const ctof = function() {
-
+const ctof = function(degsC) {
+  let toF = (degsC * 9/5 + 32).toFixed(1);
+  return parseFloat(toF);
 }
-
+//[°F] = [°C] × ​9⁄5 + 32
+ctof(0); //32
+ctof(73.2); //163.8
+ctof(-10); //14
 module.exports = {
   ftoc,
   ctof
