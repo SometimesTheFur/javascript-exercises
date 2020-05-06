fc612e3593a5ac975507f72bbfead3bf2960ea80
SometimesTheFur <timothymahr@gmail.com>
2020-05-06 13:04:37 -0400

    Tests 0 Failures

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+       return true;
+    } else if (year % 4 === 0 && year % 100 !== 0) {
+        return true; 
+    } else {
+     return false;
+    }
 }
-
+leapYears(1996);  //true
+leapYears(1997);  //false
+leapYears(34992); //true
+leapYears(1900);  //false
+leapYears(1600);  //true
+leapYears(700);   //false
 module.exports = leapYears
