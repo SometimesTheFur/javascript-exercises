c551d9e6e9a964d177c014d57fe781738c688a3b
SometimesTheFur <timothymahr@gmail.com>
2020-05-28 10:58:02 -0400

    Framework from Solutions broken. Attempting to repair

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,6 +1,24 @@
-const caesar = function(char, Shift) {
-    
-}
+const caesar = function(str, shift) {
+    return str
+        .split("")
+        .map(stringA => shifted(stringA + shift))
+        .join("");
+};
+const cypherSet = cypher => (cypher < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const shifted = (stringA, shift) => {
+    const cypher = stringA.charCodeAt();
+    if ((cypher >= 65 && cypher <= 90) || (cypher >= 97 && cypher <= 122)) {
+        return String.fromCharCode(
+            mod(cypher + shift - cypherSet(cypher), 26) + cypherSet(cypher)
+        );
+    }
+    return stringA;
+};
+
+
 
 
 module.exports = caesar
