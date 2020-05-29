ee5267adacebe0397a6a0e46d3e571924f5a42e8
SometimesTheFur <timothymahr@gmail.com>
2020-05-29 09:48:01 -0400

    Tests 0 Failures

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
-}
+const palindromes = (string) => {
+    const raw = string.toLowerCase().replace(punctuation, '');
+        return raw
+            .split("")
+            .reverse()
+            .join("") === raw       
+};
+    const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s+/]/g;
 
 module.exports = palindromes
