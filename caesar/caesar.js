d9b45df3e1d979db2fbbbfe5cb5c9460f339eada
SometimesTheFur <timothymahr@gmail.com>
2020-05-29 06:01:38 -0400

    Test 0 Failures

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,24 +1,22 @@
-const caesar = function(str, shift) {
-    return str
-        .split("")
-        .map(stringA => shifted(stringA + shift))
-        .join("");
-};
-const cypherSet = cypher => (cypher < 97 ? 65 : 97);
+const caesar = (str, shift) => {
+        return str
+            .split("")
+            .map(stringA => shifted(stringA, shift))
+            .join("");
+    };
+    const cypherSet = cypher => (cypher < 97 ? 65 : 97);
 
-const mod = (n, m) => (n % m + m) % m;
+    const mod = (n,m) => (n % m + m) % m;
 
-const shifted = (stringA, shift) => {
-    const cypher = stringA.charCodeAt();
-    if ((cypher >= 65 && cypher <= 90) || (cypher >= 97 && cypher <= 122)) {
-        return String.fromCharCode(
-            mod(cypher + shift - cypherSet(cypher), 26) + cypherSet(cypher)
-        );
-    }
-    return stringA;
-};
-
-
-
+    const shifted = (stringA, shift) => {
+        const cypher = stringA.charCodeAt();
+        
+        if ((cypher >= 65 && cypher <= 90) || (cypher >= 97 && cypher <= 122)) {
+            return String.fromCharCode(
+                mod(cypher + shift - cypherSet(cypher), 26) + cypherSet(cypher)
+            );
+        }
+        return stringA;
+    };
 
 module.exports = caesar
