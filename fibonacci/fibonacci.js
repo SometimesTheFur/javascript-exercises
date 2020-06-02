d6d54e15dff0c9cdb91bad0aba57fc5761d21020
SometimesTheFur <timothymahr@gmail.com>
2020-06-02 14:44:12 -0400

    Tests 0 Failures

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,14 +1,16 @@
 const fibonacci = function(fib) {
-    const n = parseInt(fib);
-    //const s = n.split();
+    let n = parseInt(fib);
+    let [a, b] = [0, 1];
     if(n === 1 || n === 0) {
         return n;
     }else if(n < 0) {
         return "OOPS";
     }else if(n > 1) {
-        return n.push(n[n.length - 1] + n[n.length - 2]);
+        while (--n > 0) {
+            [a, b] = [b, a + b];
+        }
+        return b;
     }
-    //const s = (n - 1);
 
     
 }
