5e8ba2d9e1bdf3981293e837c62b1ef002d378d5
SometimesTheFur <timothymahr@gmail.com>
2020-06-02 14:58:45 -0400

    Init Commit- working object

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
 const getTheTitles = function() {
-
+    return books.title;
 }
-
+const books = [
+    {title: 'Book', author: 'Name'},
+    {title: 'Book2', author: 'Name2'}
+    ]
 module.exports = getTheTitles;
