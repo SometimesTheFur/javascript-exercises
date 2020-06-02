130e8145097987f670fa76200d9df290785a3080
SometimesTheFur <timothymahr@gmail.com>
2020-06-02 14:06:10 -0400

    Working the if statement

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,7 +1,13 @@
 const fibonacci = function(fib) {
     const n = parseInt(fib);
     const s = n.split();
-    (s === 1) ? 1 : s.push(s[s.length - 1] + s[s.length - 2]);
+    if(n === 1) {
+        return [0, 1];
+    }else if(n < 0) {
+        return "OOPS";
+    }else if(n > 1) {
+        return s.push(n[n.length - 1] + n[n.length - 2]);
+    }
     //const s = (n - 1);
 
     
