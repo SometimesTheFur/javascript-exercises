db7d7924237ee098b730e913b0ef4d9db3b0ec98
SometimesTheFur <timothymahr@gmail.com>
2020-06-02 14:09:57 -0400

    Got neg, 0, and 1 tests working

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,12 +1,12 @@
 const fibonacci = function(fib) {
     const n = parseInt(fib);
-    const s = n.split();
-    if(n === 1) {
-        return [0, 1];
+    //const s = n.split();
+    if(n === 1 || n === 0) {
+        return n;
     }else if(n < 0) {
         return "OOPS";
     }else if(n > 1) {
-        return s.push(n[n.length - 1] + n[n.length - 2]);
+        return n.push(n[n.length - 1] + n[n.length - 2]);
     }
     //const s = (n - 1);
 
