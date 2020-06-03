7823e66d7a2dd30afa6356e1dbd995b016ad20d7
SometimesTheFur <timothymahr@gmail.com>
2020-06-03 17:08:07 -0400

    Going back and forth with sort and reduce

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,10 +1,14 @@
-let findTheOldest = function(arr) {
-    const oldest = arr.sort((a,b) => {
+let findTheOldest = (person) => {
+    const person = arr.sort((a, b) => {
         const last = a.yearOfDeath - a.yearOfBirth;
-        const next = b.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
         return last > next ? -1 : 1;
     });
-    return arr
-} 
+    //const getAge = person.yearOfDeath - person.yearOfBirth;
+    // const oldest = getAge.reduce((a,b) => {
+    //     return a + (b.yearOfDeath - b.yearOfBirth);
 
+    // }, 0);
+    return ;
+};
 module.exports = findTheOldest
