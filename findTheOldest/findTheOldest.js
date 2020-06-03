57047d0b1263ffcde87b8282dc43ce9146247106
SometimesTheFur <timothymahr@gmail.com>
2020-06-03 10:49:37 -0400

    Trying Sort Method

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-let findTheOldest = function() {
-
-}
+let findTheOldest = function(arr) {
+    const oldest = arr.sort((a,b) => {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - a.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    return arr
+} 
 
 module.exports = findTheOldest
