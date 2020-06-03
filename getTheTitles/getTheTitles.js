6bdbb5605e63b36d1b8bd1b4254be7a29303c847
SometimesTheFur <timothymahr@gmail.com>
2020-06-02 20:47:01 -0400

    Tests 0 Failures!

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,7 +1,4 @@
-    const books = [
-    {title: 'Book', author: 'Name'},
-    {title: 'Book2', author: 'Name2'}
-    ];
-const getTheTitles = () => books.map(book => `${book.title}`);
+
+const getTheTitles = (arr) => arr.map(book => `${book.title}`);
 
 module.exports = getTheTitles;
