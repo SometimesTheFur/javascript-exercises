e0c2a912131056f839067cb3c8a9d3f93b2c4174
SometimesTheFur <timothymahr@gmail.com>
2020-06-08 10:35:38 -0400

    Solution tried and passed. Going back to reduce.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,7 +1,7 @@
-let findTheOldest = function(arr) {
-    arr.sort((a,b) => {
-        const last = a.yearOfDeath - a.yearOfBirth;
-        const next = b.yearOfDeath - a.yearOfBirth;
+let findTheOldest = (arr) => {
+    arr.reduce((a,b) => {
+        const last = (a.yearOfDeath - a.yearOfBirth);
+        const next = (b.yearOfDeath - a.yearOfBirth);
         return last > next ? -1 : 1;
     });
     //console.log(oldest)
